6b2626ac81337aba903289758a044c9495cb5d28
SMIAPPS <skrillitmediainc.apps@gmail.com>
2014-10-05 15:03:40 +0100

    Created A Rails Application And Added Status Updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMISocial::Application.config.secret_key_base = '695da9be286b22f0b1a1350268981e75fcccda1078f4de2cbf06e7c33911b3f9c3087a57c7c46bd5222c654589ce180e6a3c9aea345620f4cdee2f633426c832'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SMISocial::Application.config.session_store :cookie_store, key: '_SMI_Social_session'
